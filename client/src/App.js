a91e14182af77524a505005eebbe07bcf8e1d6f4
FreedomWriter <natalienetworks@gmail.com>
2019-11-08 20:23:15 -0600

    class componet written, fetch written

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div>hello from app</div>;
+class App extends React.Component {
+  state = {
+    player: {}
+  };
+  componentDidMount() {
+    fetch("http://localhost:5000/api/players")
+      .then(res => res.json())
+      .then(res => {
+        // console.log(res);
+        this.setState({
+          player: res
+        });
+      })
+      .catch(err => {
+        console.log(`App: componentDidMount: err: `, err);
+      });
+  }
+  render() {
+    console.log(`app: this.state.player: `, this.state.player);
+    return <div>hello from app</div>;
+  }
 }
 
 export default App;
